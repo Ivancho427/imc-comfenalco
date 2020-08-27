39a9960ba1a97dbad14b75631f9bfa23ee116d20
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-08-27 14:29:18 -0500

    Updates

---
+++ precache-manifest.3dade9dca552f25e61e426598f0dd4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38649d877b5f013976fbd2efbbe62291",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "22209c2f1e6a04da8de7",
+    "url": "/imc-comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6c3aba8de11282b1382",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.09d32d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22209c2f1e6a04da8de7",
+    "url": "/imc-comfenalco/static/js/main.e72679a1.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
