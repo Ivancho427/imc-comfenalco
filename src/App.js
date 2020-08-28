cb1ea8fd726c11c351046f24a5ee20b010473123
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-08-28 13:24:22 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import CicloDeVida from './components/CicloDeVida'
+import { BrowserRouter, Switch, Route, Redirect, Link } from 'react-router-dom'
+import 'bulma/css/bulma.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+import CalculadorApp from './components/imc/captura/CalculadorApp'
+import DetalleIMC from './components/imc/proyeccion/DetalleIMCApp'
+export default class App extends React.Component {
+  componentWillMount() {
+    this.setState({ imc: 0 })
+  }
 
-function App() {
-  return (
-    <div className="App">
-        <CicloDeVida/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+      <BrowserRouter>
+        <div className="tabs">
+          <ul>
+            <li className="is-active"><Link to = "/calculo-imc">Calculo</Link></li>
+            <li><Link to = "/detalle-imc">Detalle</Link></li>
+          </ul>
+        </div>
+        
+          <Switch>
+            <Route path = "/calculo-imc" exact component = {() =>  <CalculadorApp App={this} /> } />
+            <Route path = "/detalle-imc" exact component = {() =>  <DetalleIMC App={this} /> } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
